fcd2145b6d766f2c2c51024089c4fb95c1131aaf
Peter Cheng <teed7334@gmail.com>
2017-02-07 23:53:17 +0800

    移除沒在用的Facebook Message Bot

--- app/robot.py
+++ app/robot.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 class robot:
 
     def __init__(self):
-        self.facebookAPIToken = 'EAAE8ZAlexD0ABAF03mfR0V9aKl5LZBTt2tbgIuK3kGwKSAtIv4JQJSZAdcxlUNabk4nKZBpBJ91m4v95OFu4OTGaVZB6Aq2hCjHQdiZAqNGosY5BaK3udSJtBxaUIGXLzyX4ZBczuZC8JWVhNHSfoWKWiloHXxaR0rEuyhkRSsuSggZDZD'
         self.driver = webdriver.PhantomJS()
 
     def setMonitorURL(self, url = []):
@@ -37,19 +36,9 @@ class robot:
                 print(title + ':' + status)
                 break
 
-    def sendFBMessage(self, to, message):
-        post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={token}'.format(token=self.facebookAPIToken)
-        response_message = json.dumps({"recipient":{"id": to},
-                                   "message":{"text":message}})
-        req = requests.post(post_message_url,
-            headers={"Content-Type": "application/json"},
-            data=response_message)
-        print("[{}] Reply to {}: {}", req.status_code, to, message)
-
 
 if __name__ == '__main__':
     urls = ['https://online.senao.com.tw/Product/400301670032', 'https://online.senao.com.tw/Product/420204289032'];
     a = robot()
-    a.sendFBMessage('1368226231', '測試');
-    #a.setMonitorURL(urls)
-    #a.analysisPage()
+    a.setMonitorURL(urls)
+    a.analysisPage()
